db6bdbcafc7289aa296e31bca6cc4261185193d5
Andy Pereira <andrewj.pereira@gmail.com>
2013-09-23 20:17:28 -0500

    Initial Commit

---
+++ BeerTracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BeerTracker
+//
+//  Created by Andy on 9/23/13.
+//  Copyright (c) 2013 Andy Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeerTracker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BeerTracker
+//
+//  Created by Andy on 9/23/13.
+//  Copyright (c) 2013 Andy Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
